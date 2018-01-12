fe7e6700d535f4540350fac294a857b405caf727
Tahir Jan <stahiralijan@gmail.com>
2018-01-12 17:11:23 +0500

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,125 @@
+## Laravel RequestCaster
+####Requirements: I've only tested this package with Laravel 5.5, please help me by testing this package in older versions of Laravel
+
+Let's learn from an example:
+
+You want to be able to save the submitted data but you don't want to make a mess in the controller method like this:
+```php
+public function store(UserFormRequest $request)
+{
+    ...
+    $first_name = ucfirst($request->first_name); // or ucfirst($request->get('first_name')
+    $last_name = ucfirst($request->last_name); // or ucfirst($request->get('last_name') 
+    ...
+    $user = User::create([
+        ...
+        'first_name' => $first_name,
+        'last_name' => $last_name,
+        ...
+    ]);
+    ...
+    // after handling model stuff
+    return redirect(route('users.index'))
+            ->with('message'=>"User ({$user->first_name} {$user->last_name}) created!");
+}
+```
+
+As you can see after a while you start to wondering what if there is a way you could automate this process so that 
+your controller would look elegant and clean. With this package you can just do that:
+
+####Step 1:
+Use the `RequestCaster` Trait in your form request (in this case `UserFormRequest`):
+```php
+...
+use Stahiralijan\RequestCaster\Traits\RequestCasterTrait;
+...
+class UserFormRequest extends FormRequest
+{
+    use RequestCasterTrait;
+    ...
+}
+```
+
+####Step 2:
+Define the Request attributes that are required to be casted:
+```php
+class UserFormRequest extends FormRequest
+{
+    use RequestCasterTrait;
+    
+    protected $toUCFirstWords = ['first_name','last_name'];
+    
+    // More about this is explained below
+    protected $joinStrings = ['fullname'=>' |first_name,last_name'];
+    ...
+}
+```
+####Finally
+...and that's all you needed to do, `first_name` and `last_name` are automatically capitalized. Also, you don't need to worry about your form data being getting dirty before validation because these castings will run after the validator validates the form data. 
+```php
+public function store(UserFormRequest $request)
+{
+    // first_name and last_name  
+    $user = User::create($request->all());
+    ...
+    // after handling model stuff
+    return redirect(route('users.index'))
+            ->with('message'=>"User ({$request->full_name}) created!");
+}
+```
+###Installation
+Install this package by typing the following command:
+```shell
+composer require stahiralijan/request-caster
+```
+
+###Available transformations / Casts
+The following casts are available: 
+ - `$toLowerCaseWords`: Applies `strtolower()` to the selected field(s).
+ - `$toUpperCaseWords`: Applies `strtoupper()` to the selected field(s).
+ - `$toUCFirstWords`: Applies `ucwords()` to the selected field(s).
+ - `$toSlugs`: Applies `str_slug()` to the selected field(s).
+ - `$toIntegers`: Casts selected field(s) to `int`.
+ - `$toFloats`: Casts selected field(s) to `float`.
+ - `$toBooleans`: Casts selected field(s) to `bool`.
+ - `$toArrayFromJson`: Applies `json_decode()` to the selected fields.
+
+###Available methods
+For now only `collection` method is available
+
+You can use this method to get a collection (`Illuminate\Support\Collection`) of all the attributes
+
+```php
+public function store(UserFormReques $request)
+{
+    $request->collection()->filter(function($item){
+        ...
+    });
+    // or
+    $request->collection()->map(function($item){
+        ...
+    });
+}
+```
+### How to cast
+All of the properties are pretty straight forward, you define the attributes that needs to be casted like this:
+```php
+// Convert the defined attributes to Upper-case
+$toUpperCaseWords = ['product_code'];
+
+// Upper-case the first letter of the words defined below
+$toUCFirstWords = ['display_name'];
+
+// Convert the following attributes into slugs
+$toSlugs = ['product_name'];
+``` 
+You got the idea about the usage of the simple stuff, now one special transformation / caster
+```php
+$joinStrings = ['fullname'=>' |first_name,last_name'];
+```
+ - Here `fullname` will be a new attribute of the `FormRequest` which does not exists in the either the form or the FormRequest in the current context.
+ - Notice a space `' '` in the starting of value is the glue of the two attributes
+ - Next `|` is the separator between the glue and the desired attributes
+ - Next you add the attributes that needs to be glued.
+
+If `first_name` is `Tahir` and `last_name` is `Jan` the output will be `Tahir Jan` according to the above rule, and can be accessed with `$request->fullname` or `$request->get('fullname')`
---
+++ src/Traits/RequestCasterTrait.php
@@ -0,0 +1,197 @@
+<?php
+
+    namespace Stahiralijan\RequestCaster\Traits;
+
+    use Illuminate\Support\Collection;
+
+    /**
+     * Trait RequestCasterTrait
+     *
+     * @property array $toLowerCaseWords
+     * @property array $toUpperCaseWords
+     * @property array $toUCFirstWords
+     * @property array $toSlugs
+     * @property array $toIntegers
+     * @property array $toFloats
+     * @property array $toBooleans
+     * @property array $toArrayFromJson
+     * @package App\Http\Requests\Traits
+     */
+    trait RequestCasterTrait
+    {
+        /**
+         * @param array|null $keys
+         *
+         * @return \Illuminate\Support\Collection
+         */
+        public function collection(array $keys = NULL)
+        {
+            return Collection::make($this->all($keys));
+        }
+
+        public function validate()
+        {
+            $this->prepareForValidation();
+
+            $instance = $this->getValidatorInstance();
+
+            if (!$this->passesAuthorization())
+            {
+                $this->failedAuthorization();
+            } else if (!$instance->passes())
+            {
+                $this->failedValidation($instance);
+            }
+
+            if ($instance->passes())
+            {
+                $this->mapCasts();
+            }
+        }
+
+        public function mapCasts(): void
+        {
+            $this->castToLowerCaseWords();
+            $this->castToUpperCaseWords();
+            $this->castUCFirstWords();
+            $this->castToSlugs();
+            $this->castToInteger();
+            $this->castToFloats();
+            $this->castToBoolean();
+            $this->castJsonToArray();
+
+            // run this in last so if there are fields that need to be run first should run first
+            // i.e. names should be capitalized before joined together
+            $this->castJoinFields();
+        }
+
+        protected function castJoinFields(): void
+        {
+            if (property_exists($this, 'joinStrings') && $this->joinStrings)
+            {
+                foreach ($this->joinStrings as $newFieldName => $value)
+                {
+                    $newFields = explode('|', $value);
+                    $glue = $newFields[0];
+                    $newFields = explode(',', $newFields[1]);
+                    if ($newFields)
+                    {
+                        $joinedField = implode($glue, $this->all(array_values($newFields)));
+                        $this->request->set($newFieldName, $joinedField);
+                    }
+                }
+            }
+        }
+
+        protected function castToLowerCaseWords(): void
+        {
+            if (property_exists($this, 'toLowerCaseWords') && $this->toLowerCaseWords)
+            {
+                foreach ($this->toLowerCaseWords as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, strtolower(request($key)));
+                    }
+                }
+            }
+        }
+
+        protected function castToUpperCaseWords(): void
+        {
+            if (property_exists($this, 'toUpperCaseWords') && $this->toUpperCaseWords)
+            {
+                foreach ($this->toUpperCaseWords as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, strtoupper(request($key)));
+                    }
+                }
+            }
+        }
+
+        protected function castUCFirstWords(): void
+        {
+            if (property_exists($this, 'toUCFirstWords') && $this->toUCFirstWords)
+            {
+                foreach ($this->toUCFirstWords as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, ucwords(request($key)));
+                    }
+                }
+            }
+        }
+
+        protected function castToSlugs(): void
+        {
+            if (property_exists($this, 'toSlugs') && $this->toSlugs)
+            {
+                foreach ($this->toSlugs as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, str_slug(request($key)));
+                    }
+                }
+            }
+        }
+
+        protected function castToInteger()
+        {
+            if (property_exists($this, 'toIntegers') && $this->toIntegers)
+            {
+                foreach ($this->toIntegers as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, (int)request($key));
+                    }
+                }
+            }
+        }
+
+        protected function castToFloats(): void
+        {
+            if (property_exists($this, 'toFloats') && $this->toFloats)
+            {
+                foreach ($this->toFloats as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, (float)request($key));
+                    }
+                }
+            }
+        }
+
+        protected function castToBoolean(): void
+        {
+            if (property_exists($this, 'toBooleans') && $this->toBooleans)
+            {
+                foreach ($this->toBooleans as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, (bool)request($key));
+                    }
+                }
+            }
+        }
+
+        protected function castJsonToArray(): void
+        {
+            if (property_exists($this, 'toArrayFromJson') && $this->toArrayFromJson)
+            {
+                foreach ($this->toArrayFromJson as $key)
+                {
+                    if ($this->request->has($key))
+                    {
+                        $this->request->set($key, json_decode(request($key)));
+                    }
+                }
+            }
+        }
+    }
